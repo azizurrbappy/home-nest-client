fa03cc818ee7f837f490b07d2c09e2385e54003f
azizurrbappy <azizurr.bappy@gmail.com>
2025-11-09 00:42:32 +0600

    added Featured Real Estate

---
+++ src/components/FeaturedRealEstate/FeaturedRealEstateCard.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { BiBath } from 'react-icons/bi';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import { FiMapPin } from 'react-icons/fi';
+import { GoArrowRight } from 'react-icons/go';
+import { MdBed } from 'react-icons/md';
+import { SlSizeFullscreen } from 'react-icons/sl';
+import { Link } from 'react-router';
+
+const FeaturedRealEstateCard = ({ data }) => {
+  const [isFav, setIsFav] = useState(false);
+
+  const {
+    propertyImage,
+    propertyPrice,
+    propertyName,
+    shortDescription,
+    location,
+    beds,
+    bathroom,
+    flatSize,
+  } = data;
+
+  const addEllipsisAfterWords = (text, wordCount = 3) => {
+    const words = text.split(' ');
+
+    if (words.length <= wordCount) {
+      return text;
+    }
+
+    const limitedWords = words.slice(0, wordCount);
+    return limitedWords.join(' ') + '...';
+  };
+
+  return (
+    <>
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <figure
+          style={{
+            backgroundImage: `url('${propertyImage}')`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            height: '200px',
+            width: '100%',
+          }}
+        ></figure>
+        <div className="card-body">
+          <div className="flex items-center justify-between">
+            <h2 className="text-2xl font-extrabold text-[#7065F0]">
+              ${propertyPrice}
+              <span className="text-gray-500 text-base font-medium">
+                /month
+              </span>
+            </h2>
+
+            <span
+              onClick={() => setIsFav(!isFav)}
+              className="p-3 border border-[#E8E6F9] rounded-full cursor-pointer"
+            >
+              {isFav ? (
+                <FaHeart size={18} color="#7065F0" />
+              ) : (
+                <FaRegHeart size={18} color="#7065F0" />
+              )}
+            </span>
+          </div>
+          <h2 className="text-2xl font-extrabold text-[#000929]">
+            {propertyName}
+          </h2>
+          <p className="text-gray-500">
+            {addEllipsisAfterWords(shortDescription, 10)}
+          </p>
+          <p className="text-gray-500 flex items-center gap-2">
+            <FiMapPin /> 2821 Lake Sevilla, Palm Harbor, TX
+          </p>
+          <hr className="my-4 text-gray-200" />
+          <div className="flex justify-between items-center">
+            <span className="flex items-center gap-2">
+              <MdBed color="#7065F0" size={20} /> {beds} Beds
+            </span>
+            <span className="flex items-center gap-2">
+              <BiBath color="#7065F0" size={20} /> {bathroom} Bathrooms
+            </span>
+            <span className="flex items-center gap-2">
+              <SlSizeFullscreen color="#7065F0" size={17} /> {flatSize}
+            </span>
+          </div>
+          <Link className="mt-5 mx-auto text-[#000929] flex items-center hover:gap-2 hover:text-[#7065F0] duration-200 hover:underline">
+            View Details <GoArrowRight size={18} />
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FeaturedRealEstateCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Slider from '../components/Slider/Slider';
 import Container from '../components/Container/Container';
+import FeaturedRealEstateCard from '../components/FeaturedRealEstate/FeaturedRealEstateCard';
+import useAxios from '../hooks/useAxios';
 
 const Home = () => {
+  const axios = useAxios();
+  const [freData, setFREData] = useState();
+
+  useEffect(() => {
+    axios('/featuredrealestate').then(data => {
+      setFREData(data.data);
+    });
+  }, []);
+
   return (
     <>
       <Container>
         <Slider></Slider>
       </Container>
 
-      <Container></Container>
+      <Container className="my-20">
+        <div className="w-fit mx-auto text-center mb-16">
+          <h1 className="text-[#000929] font-bold lg:text-4xl text-3xl mb-2">
+            Featured Real Estate
+          </h1>
+          <p className="text-[#000929]">
+            Some of our picked properties near you location.
+          </p>
+        </div>
+
+        <section className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 w-fit mx-auto">
+          {freData
+            ? freData.map(data => (
+                <div key={data._id}>
+                  <FeaturedRealEstateCard data={data}></FeaturedRealEstateCard>
+                </div>
+              ))
+            : ''}
+        </section>
+      </Container>
     </>
   );
 };
